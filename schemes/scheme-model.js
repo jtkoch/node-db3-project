a354e58d1d7c62fdd15591f76ebf335634f1f78c
Jensen Koch <jtkoch12@gmail.com>
2020-04-28 13:59:00 -0500

    Finished MVP

---
+++ schemes/scheme-model.js
@@ -0,0 +1,44 @@
+const db = require("../data/config")
+
+function find() {
+  return db("schemes")
+}
+
+function findById(id) {
+  return db("schemes")
+  .where({ id })
+  .first()
+}
+
+function findSteps(scheme_id) {
+  return db("steps")
+  .join("schemes", "schemes.id", "steps.scheme_id")
+  .select("steps.id", "schemes.scheme_name", "steps.instructions")
+  .where({ scheme_id })
+}
+
+function add(newScheme) {
+  return db("schemes")
+  .insert(newScheme)
+}
+
+function update(changes, id) {
+  return db("schemes")
+  .where({ id })
+  .update(changes)
+}
+
+function remove(id) {
+  return db("schemes")
+  .where({ id })
+  .del()
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+}
